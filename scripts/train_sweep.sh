bcb1036e3141d8b82357e75ef1d63f83880a154f
kcz358 <kaichenzhang358@outlook.com>
2024-11-01 09:37:29 +0000

    Add sample config and script for sweep params

---
+++ scripts/train_sweep.sh
@@ -0,0 +1,24 @@
+# export WANDB_API_KEY=<YOUR_WANDB_KEY>
+# export WANDB_PROJECT=<YOUR_PROJECT>
+# export WANDB_ENTITY=<YOUR_USER_NAME>
+# export WANDB_MODE=online
+
+RUN_NAME=train_rnn
+
+python3 -m sc4002.train.train_sweep \
+    --output_dir ./checkpoints \
+    --report_to wandb \
+    --model_type rnn \
+    --eval_strategy steps \
+    --eval_steps 100 \
+    --logging_steps 10 \
+    --label_names "labels" \
+    --learning_rate 1e-5 \
+    --num_train_epochs 10 \
+    --freeze_word_embed \
+    --sweep_config ./scripts/train/sweep_config.json \
+    --sweep_count 5 \
+    --run_name $RUN_NAME
+    # --lr_scheduler_type "cosine" \
+    # --weight_decay 0. \
+    # --warmup_ratio 0.03
