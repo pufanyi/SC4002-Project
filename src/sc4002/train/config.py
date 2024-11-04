7b060820761bc05f375ba451c36d946bb97d70cc
Kaichen Zhang - NTU <kaichenzhang358@outlook.com>
2024-11-04 11:24:28 +0800

    [Feat] Add params sweep, add masks into RNN to ignore the pad tokens (#4)
    * Update dependency
    
    * Add mask to ignore pad tokens
    
    * Refactor code
    
    * Fix return
    
    * Refactor training args
    
    * Add sample config and script for sweep params
    
    * Update gitignore
    
    * Add train sweep
    
    * Lint

---
+++ src/sc4002/train/config.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from transformers import TrainingArguments
+
+
+@dataclass
+class ModelArguments:
+    model_type: str = field(default=None)
+    input_size: int = field(default=300)
+    hidden_size: int = field(default=512)
+    download_repo: str = field(default="kcz358/glove")
+    tokenizer_path: str = field(default="glove.6B/glove.6B.300d.tokenizer.json")
+    word_embed_path: str = field(default="glove.6B/glove.6B.300d.safetensors")
+    freeze_word_embed: bool = field(default=False)
+
+
+@dataclass
+class DataArguments:
+    dataset_name: str = field(default="rotten_tomatoes")
+    train_split: str = field(default="train")
+    val_split: str = field(default="validation")
+    test_split: str = field(default="test")
+
+
+@dataclass
+class CustomTrainingArguments(TrainingArguments):
+    sweep_config: Optional[str] = field(default=None)
+    sweep_count: Optional[int] = field(default=20)
